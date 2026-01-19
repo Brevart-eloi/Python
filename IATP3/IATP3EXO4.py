0476aac911a56074b6fd6d310409c778c9f0bb6a
Brevart-eloi <eloi.brevart@gmail.com>
2026-01-19 15:33:55 +0100

    ajout de input

--- IATP3/IATP3EXO4.py
+++ IATP3/IATP3EXO4.py
@@ -1,8 +1,8 @@
 
 import IATP3EXO4_fonction
-num1 = 10
-num2 = 25
-num3 = 15
+num1 = input("Entrez le premier nombre : ")
+num2 = input("Entrez le deuxième nombre : ")
+num3 = input("Entrez le troisième nombre : ")
 max = IATP3EXO4_fonction.maximum(num1, num2, num3)
 print("Le maximum de", num1, ",", num2, ",", num3, "est :", max)
 
@@ -18,10 +18,24 @@ function maximum(a, b, c) {
         return c;
     }
 }
-
-const num1 = 10;
-const num2 = 25;
-const num3 = 15;
-const max = maximum(num1, num2, num3);
-console.log("Le maximum de", num1, ",", num2, "et", num3, "est :", max);
+const input1 = document.createElement('input');
+const input2 = document.createElement('input');
+const input3 = document.createElement('input');
+const bouton = document.createElement('button');
+input1.id = 'input1';
+input2.id = 'input2';
+input3.id = 'input3';
+bouton.id = 'bouton';
+bouton.textContent = "bouton : maximum de 3 nombres";
+document.body.appendChild(input1);
+document.body.appendChild(input2);
+document.body.appendChild(input3);
+document.body.appendChild(bouton);
+bouton.onclick = () => {
+  const num1 = Number(input1.value);
+  const num2 = Number(input2.value);
+  const num3 = Number(input3.value);
+  const max = maximum(num1, num2, num3);
+  console.log("Le maximum de", num1, ",", num2, ",", num3, "est :", max);
+};
 '''
