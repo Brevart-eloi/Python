f34e97031d0b96c7762efe1d8a144ca4e6d75c82
Brevart-eloi <eloi.brevart@gmail.com>
2026-02-02 14:47:15 +0100

    czfbhzisubfi

--- IATP4/TP4_exo1.py
+++ IATP4/TP4_exo1.py
@@ -1,5 +1,5 @@
 import pandas as pandasLib
 DataFrame = pandasLib.read_csv("donnevrai.csv")
-DataFrame["danger"] = DataFrame["danger"].map({"safe": 0, "danger": 1})
+DataFrame["danger"] = DataFrame["danger"].map({0:"safe", 1: "danger"})
 X = DataFrame["vitesse"].tolist()
 Y = DataFrame["danger"].tolist()
--- IATP4/TP4_exo2.py
+++ IATP4/TP4_exo2.py
@@ -2,7 +2,7 @@ import random
 import pandas as pandasLib
 
 DataFrame = pandasLib.read_csv("donnevrai.csv")
-DataFrame["danger"] = DataFrame["danger"].map({"safe": 0, "danger": 1})
+DataFrame["danger"] = DataFrame["danger"].map({0:"safe", 1: "danger"})
 X = DataFrame["vitesse"].tolist()
 Y = DataFrame["danger"].tolist()
 
